145c284223c4aa2e2ac9d7fafbb71b06ca1e9f5b
SararitaFranco <francofabi83@gmail.com>
2023-07-08 22:46:34 -0400

    cursodepythonconjuan

---
+++ variables.py
@@ -0,0 +1,14 @@
+#definiendo una variable
+nombre = "Sara"
+
+#concatenar con +
+bienvenida = "Hola " + " ¿Como estas?"
+
+#concatenar con f-Strings
+bienvenida = f"Hola {nombre} ¿Como estas? "
+
+#operadores de pertenencia (in / not in)
+
+print ("Sara "  in  bienvenida) #true
+
+print ("Sara " not in bienvenida) #false
